3db5984447bb3209aa7692ee9c4977f16ae3a1e2
Shiladitya Mandal <16.shiladitya@gmail.com>
2016-06-04 10:29:46 +0530

    Corrected module in proto

--- lib/proto/hodor.rb
+++ lib/proto/hodor.rb
@@ -4,15 +4,15 @@
 require 'google/protobuf'
 
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "search.HodorRequest" do
+  add_message "hodor.HodorRequest" do
     optional :message, :string, 1
   end
-  add_message "search.HodorResponse" do
+  add_message "hodor.HodorResponse" do
     optional :message, :string, 1
   end
 end
 
-module Search
-  HodorRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("search.HodorRequest").msgclass
-  HodorResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("search.HodorResponse").msgclass
+module Hodor
+  HodorRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("hodor.HodorRequest").msgclass
+  HodorResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("hodor.HodorResponse").msgclass
 end
